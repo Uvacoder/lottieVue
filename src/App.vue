6dce5a356f6d8be18f9b17a330fbbbcca983dc5d
Maarten Lauwaert <maartenlauwaert@gmail.com>
2018-11-09 19:06:24 +0100

    Added Spotify Login

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,62 @@
 <template>
   <div id="app">
+  
+  {{ me }}
+    <button @click="login">Login with Spotify</button>
+
     <HelloWorld />
   </div>
 </template>
 
 <script>
+
+
+  
+
 import HelloWorld from './components/HelloWorld.vue'
+// import { firebase } from 'firebase';
+
+
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      client_id: 'fef0c38afdaa4991b99105485070a86b',
+      scopes: 'user-top-read',
+      redirect_uri: 'localhost:8080',
+      me: null
+    }
+  },
+    methods: {
+    login() {
+      let popup = window.open(`https://accounts.spotify.com/authorize?client_id=${this.client_id}&response_type=token&redirect_uri=${this.redirect_uri}&scope=${this.scopes}&show_dialog=true`, 'Login with Spotify', 'width=800,height=600')
+      
+      window.spotifyCallback = (payload) => {        
+        popup.close()
+        
+        fetch('https://api.spotify.com/v1/me', {
+          headers: {
+            'Authorization': `Bearer ${payload}`
+          }
+        }).then(response => {
+          console.log(response)
+          return response.json()
+        }).then(data => {
+          this.me = data
+        })
+      }
+    }
+  },
+  mounted() {
+    this.token = window.location.hash.substr(1).split('&')[0].split("=")[1]
+    
+    if (this.token) {
+      window.opener.spotifyCallback(this.token)
+    }
   }
 }
 </script>
