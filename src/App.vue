6b6c064dbb3c6ff53e725479eae800c181b7548c
Maarten Lauwaert <maartenlauwaert@gmail.com>
2018-11-09 14:17:11 +0100

    changed bg color

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,9 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  background-color: #ffd862;
+} 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,5 +26,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
 </style>
